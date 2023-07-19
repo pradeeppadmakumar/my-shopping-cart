a4df822e93b6ccb8e301bd10bdbca216c95b2753
Pradeep Padmakumar <pradeep.padmakumar91@gmail.com>
2023-07-18 19:52:50 -0500

    Setting up project

---
+++ src/test/java/com/pradeeppadmakumar/myshoppingcart/MyShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradeeppadmakumar.myshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
