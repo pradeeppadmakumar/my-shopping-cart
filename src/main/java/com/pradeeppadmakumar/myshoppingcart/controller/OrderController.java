ded12d7cac254e7483ae579a788926d95d8dffa7
Pradeep Padmakumar <pradeep.padmakumar91@gmail.com>
2023-07-19 23:14:03 -0500

    added basic controller implementation

---
+++ src/main/java/com/pradeeppadmakumar/myshoppingcart/controller/OrderController.java
@@ -0,0 +1,16 @@
+package com.pradeeppadmakumar.myshoppingcart.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class OrderController {
+
+    @GetMapping("/orders")
+    public String getOrders() {
+        return "List of orders";
+    }
+
+}
--- src/main/java/com/pradeeppadmakumar/myshoppingcart/service/OrderService.java
+++ src/main/java/com/pradeeppadmakumar/myshoppingcart/service/OrderService.java
@@ -1,5 +1,7 @@
 package com.pradeeppadmakumar.myshoppingcart.service;
 
+import org.springframework.stereotype.Service;
+
 @Service
 public class OrderService {
 
